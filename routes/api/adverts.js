21a0a8ea68dbcd005aff2f6cb3508754596b9eb8
Ayelen Lafonte <ayelenlafonte@gmail.com>
2022-03-09 20:44:58 +0100

    New y detail funcionando ok

--- routes/api/adverts.js
+++ routes/api/adverts.js
@@ -5,9 +5,9 @@
 'use strict';
 const express = require("express");
 const router = express.Router();
-const Adverts = require('../../models/Adverts');
+const Advert = require('../../models/Adverts');
 
-// GET /adverts
+// GET api/adverts
 router.get("/", async (req, res, next) => {
 
   try {
@@ -19,12 +19,62 @@ router.get("/", async (req, res, next) => {
     const sort = req.query.sort;
     const filters = {};
 
-    const adverts = await Adverts.list(filters, skip, limit, select, sort);
+    const adverts = await Advert.list(filters, skip, limit, select, sort);
     console.log(adverts)
     res.json({ results: adverts })
   } catch (err) {
     next(err);
   }
 });
+
+// Detail
+// GET /api/adverts:id
+router.get('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+
+    const advert = await Advert.find({ _id: _id });
+    res.json({ result: advert });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Delete adverts
+// DELETE /api/adverts: id
+router.delete('/:id', async (req, res, next) => {
+  try {
+      const _id = req.params.id;
+
+      await Advert.deleteOne({_id: _id });
+      res.json();
+  } catch (err) {
+      next(err);
+  }
+});
+
+// Update adverts
+// PUT /api/adverts:id
+router.put('/:id', async (req, res, next) => {
+  try {
+      const _id = req.params.id;
+      const advertData = req.body;
+
+      const advertUpdate = await Advert.findOneAndUpdate({ _id: _id }, advertData, {
+          new: true // Devuelve estado final
+      });
+
+      if(!advertUpdate){
+          res.status(404).json({ error: 'not found'})
+          return;
+      }
+
+      res.json({ result: advertUpdate });
+  } catch (err) {
+      next(err);
+  }
+});
+
+
 
 module.exports = router;
---
+++ routes/api/new-advert.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const Advert = require('../../models/Adverts');
+
+// Crear nuevos anuncios
+router.post('/', async (req, res, next) => {
+  try {
+      const advertData = req.body;
+      const advert = new Advert (advertData); // Crear anuncio en memoria
+      const advertCreated = await advert.save();
+      res.status(201).json({ result: advertCreated});
+  } catch(err) {
+      next(err)
+  }
+});
+
+
+// Exportar
+module.exports = router
