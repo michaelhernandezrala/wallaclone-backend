6e075c79a021542f708f88d57614b75b66f84981
Michael Hernandez <mhernandezrala@gmail.com>
2022-03-30 19:52:07 +0200

    .

--- routes/api/new-advert.js
+++ routes/api/new-advert.js
@@ -7,30 +7,36 @@ const asyncHandler = require('express-async-handler');
 const { body } = require('express-validator');
 
 // Return the list of available tags
-router.get('/tags', asyncHandler(async function (req, res) {
-  const distinctTags = await Advert.distinct('tags');
-  res.json({ result: distinctTags });
-}));
+router.get(
+  '/tags',
+  asyncHandler(async function (req, res) {
+    const distinctTags = await Advert.distinct('tags');
+    res.json({ result: distinctTags });
+  })
+);
 
 // Crear nuevos anuncios
-router.post('/',[
-  body('tags').custom(tags => {
-    const allowed = Advert.allowedTags();
-    return tags.every(tag => allowed.includes(tag)) 
-  }).withMessage(`allowed tags ${Advert.allowedTags()}`)
-], async (req, res, next) => {
-  try {
-    const advertData = req.body;
-    console.log('advertData', advertData);
-    const advert = new Advert(advertData); // Crear anuncio en memoria
-    console.log('advert', advert);
-    const advertCreated = await advert.save();
-    console.log('advertCreated', advertCreated);
-    res.status(201).json({ result: advertCreated });
-  } catch (err) {
-    next(err);
+router.post(
+  '/',
+  [
+    body('tags')
+      .custom((tags) => {
+        const allowed = Advert.allowedTags();
+        return tags.every((tag) => allowed.includes(tag));
+      })
+      .withMessage(`allowed tags ${Advert.allowedTags()}`),
+  ],
+  async (req, res, next) => {
+    try {
+      const advertData = req.body;
+      const advert = new Advert(advertData); // Crear anuncio en memoria
+      const advertCreated = await advert.save();
+      res.status(201).json({ result: advertCreated });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 // Exportar
 module.exports = router;
